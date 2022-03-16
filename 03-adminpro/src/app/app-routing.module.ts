b7f643154a0510c4ae7c8adbdc8a645048fb0ff1
ro1814 <rnsm14@gmail.com>
2022-03-17 00:10:57 +0100

    login page styled

--- 03-adminpro/src/app/app-routing.module.ts
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -1,27 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
-{ path: 'dashboard', component: DashboardComponent},
-{ path: 'login', component: LoginComponent},
-{ path: 'register', component: RegisterComponent},
-{ path: 'progress', component: ProgressComponent},
-{ path: 'grafica1', component: Grafica1Component},
-{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-{ path: '**', component: NopagefoundComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot( routes )
-  ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
