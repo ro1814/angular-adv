4758d881d4237d8317435e2c682fd6e5dd4c2250
ro1814 <rnsm14@gmail.com>
2022-03-22 21:51:58 +0100

    creating spare components and using it with different attributes

--- 03-adminpro/src/app/pages/progress/progress.component.ts
+++ 03-adminpro/src/app/pages/progress/progress.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: [ './progress.component.css'
-  ]
+  styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  progreso: number = 40;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getPorcentaje() {
+    return `${this.progreso}%`;
   }
 
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.progreso = 100;
+      return;
+    }
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+
+    this.progreso = this.progreso + valor;
+  }
 }
