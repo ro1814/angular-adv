7bce6f3f9c7405f469422867b4254e927db029fa
ro1814 <rnsm14@gmail.com>
2022-03-25 00:19:31 +0100

    labels are working, not data. Must downgrade versions

--- 03-adminpro/src/app/pages/grafica1/grafica1.component.ts
+++ 03-adminpro/src/app/pages/grafica1/grafica1.component.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
-import { ChartData, ChartType, Color } from 'chart.js';
+
+
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [],
+  styles: [
+  ]
 })
 export class Grafica1Component {
-  public doughnutChartLabels: string[] = [
-    'Download Sales',
-    'In-Store Sales',
-    'Mail-Order Sales',
-  ];
-  public doughnutChartData: ChartData<'doughnut'> = {
-    labels: this.doughnutChartLabels,
-    datasets: [{ data: [350, 450, 100], backgroundColor: ['#9E120E', '#FF5800', '#FFb414']  }],
-  };
-  
-  public doughnutChartType: ChartType = 'doughnut';
+
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data1 =
+    [10, 15, 40]
+  ;
+
 }
--- 03-adminpro/src/app/pages/pages.module.ts
+++ 03-adminpro/src/app/pages/pages.module.ts
@@ -5,8 +5,6 @@ import { ComponentsModule } from '../components/components.module';
 import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms';
 
-import { NgChartsModule } from 'ng2-charts';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -26,6 +24,6 @@ import { SharedModule } from '../shared/shared.module';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule, FormsModule, ComponentsModule, NgChartsModule],
+  imports: [CommonModule, SharedModule, RouterModule, FormsModule, ComponentsModule],
 })
 export class PagesModule {}
